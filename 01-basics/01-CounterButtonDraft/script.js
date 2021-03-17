a941b1eb138298a7a48980d4fa0cc91ab9639140
Stas <allenwalker12349@gmail.com>
2021-03-17 23:05:01 +0300

    Счётчик по кнопке

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,16 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    increase() {
+      this.counter++;
+    },
+  },
+});
