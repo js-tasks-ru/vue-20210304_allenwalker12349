4858a98a7bf6e26cfeb8c81b1ce869a1830d53ea
Stas <allenwalker12349@gmail.com>
2021-03-27 23:34:30 +0300

    Задание CounterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,12 +1,17 @@
 export default {
   name: 'CounterButton',
 
-  // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
   // Компонент должен иметь модель
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="$emit(`increment`, count+1)">Увеличить</button>',
 };
