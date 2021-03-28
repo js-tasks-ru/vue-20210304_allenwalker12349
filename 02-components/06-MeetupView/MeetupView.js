dc89430109d1af8080b679e638486eecca266bc7
Stas <allenwalker12349@gmail.com>
2021-03-28 14:16:27 +0300

    Задание MeetupView

--- 02-components/06-MeetupView/MeetupView.js
+++ 02-components/06-MeetupView/MeetupView.js
@@ -1,26 +1,50 @@
-// import MeetupCover from './MeetupCover.js';
-// import MeetupDescription from './MeetupDescription.js';
-// import MeetupAgenda from './MeetupAgenda.js';
-// import MeetupInfo from './MeetupInfo.js';
-// import { getImageUrlByImageId } from './data.js';
+import MeetupCover from './MeetupCover.js';
+import MeetupDescription from './MeetupDescription.js';
+import MeetupAgenda from './MeetupAgenda.js';
+import MeetupInfo from './MeetupInfo.js';
+import { getImageUrlByImageId } from './data.js';
 
 export default {
   name: 'MeetupView',
+  components: {
+    MeetupCover, MeetupDescription, MeetupAgenda, MeetupInfo
+  },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    date() {
+      return new Date(this.meetup.date)
+    }
+  },
+
+  data() {
+    return {
+      imgUrl: getImageUrlByImageId(this.meetup.imageId)
+    }
+  },
 
   template: `
-    <div>
-      <!-- meetup cover -->
+    <div v-if="meetup">
+      <MeetupCover :title="meetup.title" :link="imgUrl" />
       <div class="container">
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
+            <MeetupDescription :description="meetup.description" />
 
             <h3>Программа</h3>
-            <!-- meetup agenda -->
+            <MeetupAgenda :agenda="meetup.agenda" />
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+            <MeetupInfo :organizer="meetup.organizer"
+                        :place="meetup.place"
+                        :date="date" />
           </div>
         </div>
       </div>
