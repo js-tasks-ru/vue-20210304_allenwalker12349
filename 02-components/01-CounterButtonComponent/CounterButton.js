2f9ee360b837d1636dc0b21d25e3709251fa2ebd
Stas <allenwalker12349@gmail.com>
2021-03-28 18:36:43 +0300

    Правки

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -13,5 +13,5 @@ export default {
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: '<button type="button" @click="$emit(`increment`, count+1)">Увеличить</button>',
+  template: '<button type="button" @click="$emit(`increment`, count+1)">{{count}}</button>',
 };
