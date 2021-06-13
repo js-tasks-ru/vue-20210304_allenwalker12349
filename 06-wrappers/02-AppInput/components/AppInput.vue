d78c77aa0056f34ecd2a62aeab022c86000d5e36
Stas <allenwalker12349@gmail.com>
2021-06-13 13:21:29 +0300

    AppInput

--- 06-wrappers/02-AppInput/components/AppInput.vue
+++ 06-wrappers/02-AppInput/components/AppInput.vue
@@ -2,7 +2,20 @@
   <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
     <img class="icon" />
 
-    <input ref="" class="form-control form-control_rounded form-control_sm" />
+    <component
+      ref="input"
+      v-bind="$attrs"
+      v-on="$listeners"
+      @input="$emit('input', $event.target.value)"
+      @change="$emit('change', $event.target.value)"
+      :value.prop="value"
+      :is="isTextAria"
+      class="form-control"
+      :class="{
+        'form-control_sm': small,
+        'form-control_rounded': rounded,
+      }"
+    />
 
     <img class="icon" />
   </div>
@@ -11,6 +24,24 @@
 <script>
 export default {
   name: 'AppInput',
+  inheritAttrs: false,
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    small: Boolean,
+    rounded: Boolean,
+    multiline: Boolean,
+    value: String,
+  },
+  computed: {
+    isTextAria() {
+      if (this.multiline) {
+        return 'textarea'
+      } else return 'input'
+    }
+  },
 };
 </script>
 
